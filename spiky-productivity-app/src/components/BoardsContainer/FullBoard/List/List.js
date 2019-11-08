089454fa076893a3a1476a7d9961ec211f42f57d
Ahsan <ahsanfarooq3423@gmail.com>
2019-11-08 18:53:35 +0500

    adding dropdown

--- spiky-productivity-app/src/components/BoardsContainer/FullBoard/List/List.js
+++ spiky-productivity-app/src/components/BoardsContainer/FullBoard/List/List.js
@@ -2,22 +2,51 @@ import React, {Component} from 'react';
 import Card from './Card/Card';
 import classes from './List.module.css';
 import NewCard from './Card/NewCard/NewCard';
-import Dropdown from 'react-dropdown'
-import 'react-dropdown/style.css'
+import ReactDOM from 'react-dom';
+
+
 
 class List extends Component {
 
-    options = [
-        'one', 'two', 'three'
-    ]
+    
 
-    defaultOption = this.options[0];
+    state = {
+        actions : false
+    }
+
+    componentDidMount() {
+        document.addEventListener('click', this.handleClickOutside, true);
+    }
+    
+    componentWillUnmount() {
+        document.removeEventListener('click', this.handleClickOutside, true);
+    }
+    
+    handleClickOutside = event => {
+        const domNode = ReactDOM.findDOMNode(this);
+    
+        if (!domNode || !domNode.contains(event.target)) {
+            this.setState({
+                actions: false
+            });
+        }
+    }
+
+
+
+    listActionsHandler = () => {
+        let bool = this.state.actions;
+        this.setState({actions : !bool })
+    }
 
     changeHandler = () => {
         console.log('input changed')
     }
+
+    
    
     render(){
+        
 
         let cards = [];
         if (this.props.items){
@@ -40,7 +69,16 @@ class List extends Component {
                 <div className = {classes.title} >
                     <div className = {classes.field}>{title}</div>
                     <div className = {classes.more}>
-                        <p>...</p>
+                        <p onClick = {this.listActionsHandler}>...</p>
+                        {this.state.actions ?
+                        <div className = {classes.dropdown} >
+                        <div>
+                            <div className = {classes.actions}>List Actions</div>
+                           
+                            <div className = {classes.delete}>Delete this List...</div>
+                        </div>
+                    </div>: null }
+                        
                     </div>
                 </div>
 
