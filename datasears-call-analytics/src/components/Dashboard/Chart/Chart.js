7d02b1c831d86ed30372c4d06ee643481b0fbf72
Ahsan <ahsanfarooq3423@gmail.com>
2019-11-23 20:00:11 +0500

    datasears separation

--- datasears-call-analytics/src/components/Dashboard/Chart/Chart.js
+++ datasears-call-analytics/src/components/Dashboard/Chart/Chart.js
@@ -6,54 +6,27 @@ import {Bar, Line, Pie } from 'react-chartjs-2';
 
 class Chart extends Component {
 
-    state = {
-        chartData : {
-            labels : ['Boston ', 'Worcester', 'SpingField', 'Lowell', 'Cambridge', 'New Bedford'],
-            datasets : [
-                {
-                    label :  'Population',
-                    data : [
-                        617594,
-                        181045,
-                        153063,
-                        106519,
-                        96072
-                    ],
-                    backgroundColor : [
-                        'rgba(255,99,132,0.6)',
-                        'rgba(54,153,120,0.6)',
-                        'rgba(65,21,190,0.6)',
-                        'rgba(153,76,34,0.6)',
-                        'rgba(255,54,212,0.6)',
-                        'rgba(255,23,132,0.6)'
-                    ]
-                }
-               
-
-            ]
-        }
-    }
 
     render() {
+        let show  = null;
+        if (this.props.type === 'Bar') {
+            show = (
+                <div className = {classes.chart}>
+                    <h5>Audio Calls Analytics</h5>
+                    <Bar
+                            data = {this.props.data}
+                            options = {{
+                                maintainAspectRation : false }}/>
+                </div>
+            )         
+        }
+
         return(
-            <div className = {classes.chart}>
-            <h5>Audio Calls Analytics</h5>
-
-                <Bar
-                    data = {this.state.chartData}
-                    options = {{
-                        maintainAspectRation : false
-                    }}
-
-                    />
-
-
             <div>
-
+                {show}
             </div>
-        </div>
+            
         )
-        
     }
 }
 
--- datasears-call-analytics/src/components/Dashboard/Dashboard.js
+++ datasears-call-analytics/src/components/Dashboard/Dashboard.js
@@ -4,16 +4,46 @@ import classes from './Dashboard.module.css';
 
 
 class Dashboard extends Component {
+
+    state = {
+        chartData : {
+            labels : ['گاڑی ', 'Worcester', 'SpingField', 'Lowell', 'Cambridge', 'New Bedford'],
+            datasets : [
+                {
+                    label :  'Population',
+                    data : [
+                        617594,
+                        181045,
+                        153063,
+                        106519,
+                        96072
+                    ],
+                    backgroundColor : 
+                    [
+                        'rgba(255,99,132,0.6)',
+                        'rgba(54,153,120,0.6)',
+                        'rgba(65,21,190,0.6)',
+                        'rgba(153,76,34,0.6)',
+                        'rgba(255,54,212,0.6)',
+                        'rgba(255,23,132,0.6)'
+                    ]
+                }
+               
+
+            ]
+        }
+    }
+
+
     render(){
+
         return(
             <div className = {classes.main}>
                 <div className  = {classes.title}>
                     <h3>Dashboard</h3>
                 </div>
-
-                <Chart/>
+                <Chart type = 'Bar' data = {this.state.chartData} />
                 
-
             </div>
         )
     }
