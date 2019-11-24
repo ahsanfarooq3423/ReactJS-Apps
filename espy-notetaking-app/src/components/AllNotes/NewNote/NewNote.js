abb25ce15e23a159a889e1007e23dfdaf2327d86
Ahsan <ahsanfarooq3423@gmail.com>
2019-11-24 19:51:40 +0500

    styling completed

--- espy-notetaking-app/src/components/AllNotes/NewNote/NewNote.js
+++ espy-notetaking-app/src/components/AllNotes/NewNote/NewNote.js
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './NewNote.module.css';
+import Aux from '../../../containers/hoc/Aux/Aux';
 
-const newnote = () => {
-    return(
-        <div className = {classes.main} >
-            <p>Take a note</p>
-        </div>
-    )
+class NewNote extends Component {
+
+    state = {
+        fullnote : false
+    }
+
+    fullnoteToggleHandler = () => {
+        let bool = this.state.fullnote;
+        this.setState({fullnote : !bool})
+    }
+
+
+    render(){
+        let show = (
+            <div onClick = {this.fullnoteToggleHandler} className = {classes.small} >
+                <p>Take a note</p>
+            </div>
+        )
+
+        if (this.state.fullnote) {
+            show = (
+                <div className = {classes.full}>
+                    <input className = {classes.title} placeholder = 'Title'></input>
+                    <br/>
+                    <textarea className = {classes.note} placeholder = 'Take a note...'></textarea>
+                    <br/>
+                    <div className = {classes.buttons}>
+                        <button 
+                            onClick = {this.fullnoteToggleHandler}
+                            className = {classes.cancel}>Cancel</button>
+                        <button className = {classes.done}>Done</button>
+                    </div>
+                </div>
+            )
+        }
+
+        return <Aux>
+                {show}
+              </Aux>
+        
+        
+    }
 }
 
-export default newnote;
+
+
+
+
+export default NewNote;
