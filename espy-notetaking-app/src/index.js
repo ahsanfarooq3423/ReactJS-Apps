e804c7369ed4bfb25822651674977c1dfb5f1cab
Ahsan <ahsanfarooq3423@gmail.com>
2019-11-27 21:09:48 +0500

    added logger middleware

--- espy-notetaking-app/src/index.js
+++ espy-notetaking-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import {Provider} from 'react-redux';
 
 
@@ -18,9 +18,18 @@ const rootReducer = combineReducers({
     changes : changesReducer
 })
 
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Middleware] Dispatching', action);
+            const result = next(action);
+            console.log('[Middleware] next state', store.getState())
+            return result;
+        }
+    }
+}
 
-
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render( <Provider store  = {store}><App /></Provider>, document.getElementById('root'));
 
